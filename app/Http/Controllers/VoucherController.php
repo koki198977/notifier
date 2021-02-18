073c2555aa51ff4444fa7b7c2198a0e410adf9f5
djara.dafiti <daniel.jara@dafiti.cl>
2021-02-18 09:58:01 -0300

    add print test

--- app/Http/Controllers/VoucherController.php
+++ app/Http/Controllers/VoucherController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
+use Mike42\Escpos\Printer;
 use Response;
 
 class VoucherController extends Controller
@@ -15,9 +17,27 @@ class VoucherController extends Controller
      */
     public function index(Request $request)
     {
-        $time = date('Y-m-d H:i:s');
-        $file = Storage::disk('local')->put($time.'.txt', $request->data);
-        return $file;
+        // $time = date('Y-m-d H:i:s');
+        // $file = Storage::disk('local')->put($time.'.txt', $request->data);
+        return $this->print();
+        // return $file;
+    }
+
+
+    private function print(){
+        $nombreImpresora = "CUPS-BRF-Printer";
+        $connector = new WindowsPrintConnector($nombreImpresora);
+        $impresora = new Printer($connector);
+        $impresora->setJustification(Printer::JUSTIFY_CENTER);
+        $impresora->setTextSize(2, 2);
+        $impresora->text("Imprimiendo\n");
+        $impresora->text("ticket\n");
+        $impresora->text("desde\n");
+        $impresora->text("Laravel\n");
+        $impresora->setTextSize(1, 1);
+        $impresora->text("https://parzibyte.me");
+        $impresora->feed(5);
+        $impresora->close();
     }
 
 }
