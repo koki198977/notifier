6a1e2462721e0cf565854d5739dfadaf75516ac1
djara.dafiti <daniel.jara@dafiti.cl>
2021-02-21 19:00:29 -0300

    test code pdf417

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(){ return "welcome to API";});
 // Route::post('/', 'ExampleController@index');
-// Route::post('/file', 'VoucherController@index');
+Route::get('/pdf', 'ExampleController@test');
 Route::post('/pre_cuenta', 'VoucherController@preCuenta');
 Route::post('/solicita_ticket', 'VoucherController@solicitaTicket');
 Route::post('/solicita_happy', 'VoucherController@solicitaHappy');
