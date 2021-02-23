2aabeceff24f332b73fdeb4648a0abfc275291d2
djara.dafiti <daniel.jara@dafiti.cl>
2021-02-23 18:59:42 -0300

    fx

--- app/Services/PrinterService.php
+++ app/Services/PrinterService.php
@@ -64,7 +64,7 @@ class PrinterService
         $impresora->feed();
 
         // body
-        $impresora->setTextSize(1, 2);
+        $impresora->setTextSize(1, 1);
         $impresora->text($this->set_space_col("PRODUCTO", 25) . $this->set_space_col("UNI", 3) . $this->set_space_col("PRECIO", 8, true) . $this->set_space_col("TOTAL", 12, true) . $this->jump);
         $impresora->setTextSize(1, 1);
         $impresora->text(str_repeat("_", $max_width) . $this->jump);
