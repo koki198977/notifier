7576b5c3859db0ff458484ef8e0c64efc9ef366f
djara.dafiti <daniel.jara@dafiti.cl>
2021-04-14 12:28:55 -0400

    fix

--- app/Services/PrinterService.php
+++ app/Services/PrinterService.php
@@ -66,7 +66,7 @@ class PrinterService
         if(isset($data->cli_delivery)){
             $cliente = $data->cli_delivery;
             $impresora->text("Fono:" . $this->space . $cliente['fono'] . $this->jump);
-            $impresora->text("Direccion:" . $this->space . $cliente['calle'] . $cliente['nro'] . ', ' . $cliente['poblacion'] . $this->jump);
+            $impresora->text("Direccion:" . $this->space . $cliente['direccion'] . $this->jump);
             $impresora->text("Referencia:" . $this->space . $cliente['referencia'] . $this->jump);
             $impresora->text("Forma de Pago:" . $this->space . $cliente['fpago'] . ', ' . $cliente['monto_paga'] . $this->jump);
         }
